e50454ce0228c1c7c9c5717735167ab4c4c74e11
cprice1970 <cprice1@lci1.com>
2019-03-17 15:34:20 -0400

    Added Unit Test

---
+++ FFImageLoadingMockTest.Test/Test.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+namespace FFImageLoadingMockTest.Test
+{
+    [TestFixture]
+    public class Test
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Xamarin.Forms.Mocks.MockForms.Init();
+        }
+
+        [Test]
+        public void TestCase()
+        {
+            var page = new MainPage();
+            Assert.IsNotNull(page);
+        }
+    }
+}
