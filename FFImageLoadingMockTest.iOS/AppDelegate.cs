f66dd2ffbfad8106249373f8a68bf586b193e9bf
cprice1970 <cprice1@lci1.com>
2019-03-17 15:20:31 -0400

    Initial test with FFImageloading and no unit testing

--- FFImageLoadingMockTest.iOS/AppDelegate.cs
+++ FFImageLoadingMockTest.iOS/AppDelegate.cs
@@ -22,6 +22,8 @@ namespace FFImageLoadingMockTest.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
+
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
